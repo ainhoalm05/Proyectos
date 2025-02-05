baaa26a76ec3320a2f9efea2b2c03c11821c83c3
ainhoalm05 <ainhoaleomat@gmail.com>
2025-02-05 10:33:49 +0100

    Update 05/02/2025

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-$( document ).ready(function() {
-    
+$(document).ready(function() {
     var envelope = $('#envelope');
     var btn_open = $("#open");
     var btn_reset = $("#reset");
-    
-    envelope.click( function() {
-        open();
-    });
-    btn_open.click( function() {
-        open();
-    });
-    btn_reset.click( function() {
-        close();
+    var messageBox = $(".hidden-message-box");
+
+    envelope.click(function() {
+        openEnvelope();
     });
 
-    function open() {
-        envelope.addClass("open")
-           .removeClass("close");
+    btn_open.click(function() {
+        openEnvelope();
+    });
+
+    btn_reset.click(function() {
+        closeEnvelope();
+    });
+
+    // Revelar mensaje oculto
+    messageBox.click(function() {
+        $(this).addClass("clicked");
+    });
+
+    function openEnvelope() {
+        envelope.addClass("open").removeClass("close");
     }
-    function close() {
-        envelope.addClass("close")
-           .removeClass("open");
+
+    function closeEnvelope() {
+        envelope.addClass("close").removeClass("open");
     }
-   
 });
